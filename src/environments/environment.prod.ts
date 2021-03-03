279b49c0dc42e8c63bd5d01e3e3241e6baf64c55
Max44444 <Skorokhodov.m.v@ukr.net>
2021-03-03 15:33:17 +0200

    firebase settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCi3WVwszgAIVKi84_r0Gj4zVGDxkWhz80'
 };
