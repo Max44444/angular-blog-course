1a9f77b6528b84233778f2fa8e84807dde0e53e7
Max44444 <Skorokhodov.m.v@ukr.net>
2021-03-05 16:10:58 +0200

    search and delete post

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -11,7 +11,9 @@ import {Subscription} from 'rxjs';
 export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = []
+  dSub: Subscription
   pSub: Subscription
+  searchStr = ''
 
   constructor(
     private postsService: PostsService
@@ -27,9 +29,14 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     if (this.pSub) {
       this.pSub.unsubscribe()
     }
+    if (this.dSub) {
+      this.pSub.unsubscribe()
+    }
   }
 
   remove(id: string) {
-
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id)
+    })
   }
 }
